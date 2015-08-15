91cedbc93bd0115e82a6503509aa68ca4aeef443
Albert Itaev <ijohnnyzzz@gmail.com>
2015-08-15 20:03:50 +0000

    Add environments web-console whitelisted ips

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ module Moviestore
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    #config.web_console.whitelisted_ips = '10.0.2.2'
-
     config.web_console.automount = true
   end
 end
